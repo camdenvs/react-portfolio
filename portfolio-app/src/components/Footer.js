af5918461a2d10eb8427ceae4599a49d7a65c574
Camden Van Sciver <camdenvs02@gmail.com>
2023-02-11 11:52:35 -0700

    added a Footer component and necessary images

---
+++ portfolio-app/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div>
+            <a href='https://stackoverflow.com/users/21194514/camden-van-sciver'>
+                <img src='../../public/stackoverflow768.png' alt='Stack Overflow'></img>
+            </a>
+            <a href='https://www.linkedin.com/in/camden-van-sciver-864037266/'>
+                <img src='../../public/linkedin512.png' alt='LinkedIn'></img>
+            </a>
+            <a href='https://github.com/FarmFreshYeets'>
+                <img src='../../public/github512.png' alt='GitHub'></img>
+            </a>
+        </div>
+    )
+}
